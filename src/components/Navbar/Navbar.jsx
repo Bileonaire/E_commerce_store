bd991cd09e9d397dcaa978eddc51e1f78d7ec2bf
Bileonaire <kiokoleon@gmail.com>
2021-01-11 09:49:35 +0300

    Add all shopping functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 
 import useStyles from './styles';
 import logo from '../../assets/leon.png';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
+
 
     return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
+          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
             <img src={logo} alt="commerce.js" height="25px" className={classes.image} />
             Bileonaire E_Commerce Shop
           </Typography>
           <div className={classes.grow} />
+          {location.pathname === '/' && (
           <div className={classes.button}>
-            <IconButton aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={2} color="secondary">
+            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
           </div>
+          )}
         </Toolbar>
       </AppBar>
     </>
